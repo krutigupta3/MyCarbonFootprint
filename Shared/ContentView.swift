eab3f3eec7350dbb498d87c0c7e27d8b54871404
Kruti Gupta <73044001+krutigupta3@users.noreply.github.com>
2022-10-01 11:56:32 -0400

    Initial Commit

---
+++ Shared/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Shared
+//
+//  Created by Kruti Gupta on 10/1/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Shared/MyCarbonFootprintApp.swift
@@ -0,0 +1,17 @@
+//
+//  MyCarbonFootprintApp.swift
+//  Shared
+//
+//  Created by Kruti Gupta on 10/1/22.
+//
+
+import SwiftUI
+
+@main
+struct MyCarbonFootprintApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
